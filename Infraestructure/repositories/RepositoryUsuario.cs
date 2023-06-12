172722e08562c469e389b5896581168bed956f89
Estudiante <Estudiante@ALISWPR0400>
2023-06-12 16:03:02 -0600

    Repositorios y servicios Usuario Plantilla bootswatch

---
+++ Infraestructure/repositories/RepositoryUsuario.cs
@@ -0,0 +1,27 @@
+﻿using Infraestructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestructure.Repositories
+{
+    public class RepositoryUsuario : IRepositoryUsuario
+    {
+        public Usuario GetUsuarioByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Usuario> GetUsuarios()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Usuario Guardar(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
