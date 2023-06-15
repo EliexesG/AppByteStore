c3878ccc810cccad8a4a929f0588550345dbf0ad
Estudiante <Estudiante@ALISWPR0400>
2023-06-15 14:24:28 -0600

    cambios

---
+++ Infraestructure/repositories/IRepositoryProductos.cs
@@ -0,0 +1,21 @@
+﻿using Infraestructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestructure.Repositories
+{
+    public interface IRepositoryProductos
+    {
+        IEnumerable<Producto> GetProducto();
+        Producto GetProductoByID(int id);
+        Producto GuardarProducto(Producto producto);
+        IEnumerable<Producto> GetProductoPorNombre(String nombre);
+        IEnumerable<Producto> GetProductoPorVendedor(int idProducto);
+        IEnumerable<Producto> GetProductoByCategoria(int idCategoria);
+        void DeleteProducto(int id);
+        Producto Save(Producto producto);
+    }
+}
