545ff1aa41c637cfd555447056508c92a1adf934
EliexesG <107147734+EliexesG@users.noreply.github.com>
2023-06-15 15:55:31 -0600

    Repositorio y servicio de Pedido

---
+++ ApplicationCore/Services/IServicePedido.cs
@@ -0,0 +1,18 @@
+﻿using Infraestructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Services
+{
+    public interface IServicePedido
+    {
+        IEnumerable<Pedido> GetPedidoByCliente(int idCliente);
+        IEnumerable<Pedido> GetPedidoByVendedor(int idVendedor);
+        Pedido GetPedidoByID(int id);
+        void DeletePedido(int id);
+        Pedido Save(Pedido pedido);
+    }
+}
