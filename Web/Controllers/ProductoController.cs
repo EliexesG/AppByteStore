992fc6957316f5d5a3aff7c3dccd7e0b48406c27
EliexesG <107147734+EliexesG@users.noreply.github.com>
2023-06-23 15:17:57 -0600

    Finalizado Vistas de Pedidos, factorización de productoController

--- Web/Controllers/ProductoController.cs
+++ Web/Controllers/ProductoController.cs
@@ -14,29 +14,7 @@ namespace Web.Controllers
     {
 
         private int numItemsPerPage = 6;
-
-        // GET: Producto
-        //Para el mantenimiento de productos
-        public ActionResult IndexAdmin()
-        {
-            //Creo una lista tipo Producto
-            IEnumerable<Producto> lista = null;
-            try
-            {
-                //Instancia 
-                IServiceProducto _ServiceProducto = new ServiceProducto();
-                lista = _ServiceProducto.GetProductoPorVendedor(2); //Obtengo todos los datos de la BD y los agrego a la lista 
-                ViewBag.NumItemsPerPage = numItemsPerPage;
-                ViewBag.Nombres = _ServiceProducto.GetProductoNombres(2);
-            }
-            catch (Exception ex)
-            {
-                Log.Error(ex, MethodBase.GetCurrentMethod());
-                TempData["Message"] = "Error al procesar los datos!" + ex.Message;
-                return RedirectToAction("Default", "Error");
-            }
-            return View(lista); //Retorno la vista con la lista ya cargada
-        }
+        private int idVendedor = 2;
 
         public ActionResult Index()
         {
@@ -46,10 +24,51 @@ namespace Web.Controllers
                 //Instancia 
                 IServiceProducto _ServiceProducto = new ServiceProducto();
                 lista = _ServiceProducto.GetProducto(); //Obtengo todos los datos de la BD y los agrego a la lista 
-
+                return View(lista); //Retorno la vista con la lista ya cargada
                 //Para cargar la lista de categorías
                 //Nota:Hacer repositorios de categorias
-                
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, MethodBase.GetCurrentMethod());
+                TempData["Message"] = "Error al procesar los datos!" + ex.Message;
+                return RedirectToAction("Default", "Error");
+            }
+        }
+
+        public ActionResult IndexAdmin()
+        {
+            IEnumerable<Producto> lista = null;
+            try
+            {
+                //Instancia 
+                IServiceProducto _ServiceProducto = new ServiceProducto();
+                lista = _ServiceProducto.GetProducto(); //Obtengo todos los datos de la BD y los agrego a la lista 
+                return View(lista); //Retorno la vista con la lista ya cargada
+                //Para cargar la lista de categorías
+                //Nota:Hacer repositorios de categorias
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, MethodBase.GetCurrentMethod());
+                TempData["Message"] = "Error al procesar los datos!" + ex.Message;
+                return RedirectToAction("Default", "Error");
+            }
+        }
+
+        // GET: Producto
+        //Para el mantenimiento de productos
+        public ActionResult IndexVendedor()
+        {
+            //Creo una lista tipo Producto
+            IEnumerable<Producto> lista = null;
+            try
+            {
+                //Instancia 
+                IServiceProducto _ServiceProducto = new ServiceProducto();
+                lista = _ServiceProducto.GetProductoPorVendedor(idVendedor); //Obtengo todos los datos de la BD y los agrego a la lista 
+                ViewBag.NumItemsPerPage = numItemsPerPage;
+                ViewBag.Nombres = _ServiceProducto.GetProductoNombres(idVendedor);
             }
             catch (Exception ex)
             {
@@ -61,7 +80,7 @@ namespace Web.Controllers
         }
 
         //Filtro por Nombre
-        public PartialViewResult buscarProductoxNombre(string filtro)
+        public PartialViewResult buscarProductoxNombre(string filtro, int tipoUsuario)
         {
             IEnumerable<Producto> lista = null;
             IServiceProducto _ServiceProducto = new ServiceProducto();
@@ -69,12 +88,12 @@ namespace Web.Controllers
             //Validar si hay texto vacio
             if (string.IsNullOrEmpty(filtro.Trim()))
             {
-                return PaginacionYOrden(1, 0);
+                return PaginacionYOrden(1, tipoUsuario, 0);
 
             }
             else
             {
-                lista = _ServiceProducto.GetProductoPorNombre(filtro, 2);
+                lista = _ServiceProducto.GetProductoPorNombre(filtro, idVendedor);
                 return PartialView("_PaginacionYOrdenViewProducto", lista);
 
             }
@@ -82,22 +101,33 @@ namespace Web.Controllers
         }
 
         //Para la paginación
-        public PartialViewResult PaginacionYOrden(int pag, int orden = 0)
+        public PartialViewResult PaginacionYOrden(int pag, int tipoUsuario, int orden = 0)
         {
 
             //Creo una lista tipo Producto
             IEnumerable<Producto> lista = null;
+
             try
             {
+
                 //Instancia 
                 IServiceProducto _ServiceProducto = new ServiceProducto();
-                lista = _ServiceProducto.GetProductoPorVendedor(2);
-                //
+
+                if (tipoUsuario == 1)
+                {
+
+                    lista = _ServiceProducto.GetProducto();
+                }
+                else
+                {
+                    lista = _ServiceProducto.GetProductoPorVendedor(idVendedor);
+                }
+
                 int startIndex = (pag * numItemsPerPage) - numItemsPerPage;
                 int count = (lista.Count() - startIndex) < numItemsPerPage ? (lista.Count() - startIndex) : numItemsPerPage;
                 lista = ((List<Producto>)lista).GetRange(startIndex, count);
 
-                if(orden == 0)
+                if (orden == 0)
                 {
                     lista = lista.OrderByDescending(producto => producto.Precio);
                 }
@@ -105,7 +135,7 @@ namespace Web.Controllers
                 {
                     lista = lista.OrderBy(producto => producto.Precio);
                 }
-                
+
             }
             catch (Exception ex)
             {
@@ -114,81 +144,6 @@ namespace Web.Controllers
             }
 
             return PartialView("_PaginacionYOrdenViewProducto", lista);
-        }
-
-
-        // GET: Producto/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-        
-       
-
-        // GET: Producto/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Producto/Create
-        [HttpPost]
-        public ActionResult Create(FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Producto/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Producto/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Producto/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Producto/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
         }
     }
 }
